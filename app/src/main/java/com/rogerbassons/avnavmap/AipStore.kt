82040fd04fd5dde3b62cf96a1722ad8aea1e3dfb
Roger Bassons <rogerbassons@gmail.com>
2022-06-05 21:22:19 +0200

    json parser

---
+++ app/src/main/java/com/rogerbassons/avnavmap/AipStore.kt
@@ -0,0 +1,13 @@
+package com.rogerbassons.avnavmap
+
+class AipDownloader {
+    val countries: Collection<String>
+
+    init {
+        countries = mutableSetOf("es")
+    }
+
+    
+
+
+}
---
+++ app/src/main/java/com/rogerbassons/avnavmap/Airspace.kt
@@ -0,0 +1,4 @@
+package com.rogerbassons.avnavmap
+
+class Airspace {
+}
